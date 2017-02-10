402418c9750886cd4c603e33d1f6a2c68a93c30e
Frederico Sales <fbiela@gmail.com>
2017-02-10 11:42:55 -0200

    wsgi debian

--- blueprint.wsgi
+++
@@ -1,8 +0,0 @@
-"""Import."""
-import sys
-
-# path
-sys.path.insert(0,"/home/frederico/virtualenvs/blueprint/app")
-
-# application
-from run import app as application
--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,8 @@
-blueprint.wsgi
+"""Import."""
+import sys
+
+# path
+sys.path.insert(0,"/home/frederico/virtualenvs/blueprint/app")
+
+# application
+from run import app as application
